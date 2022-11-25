77fec1ad5a15a7ed8bd6af32a50ee9b53218e6f9
hieu.pham <hieu.pham@empired.com>
2022-11-25 20:18:23 +0700

    header validation for promo-so-validator

---
+++ wf-promo-so-validator-new/header-validator.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+
+fs.readFile("./data-test-kit/events-gus.json","utf8",(err, data) =>{
+    if(err){
+        console.log("File read failed:", err);
+        return;
+    }
+
+    var orderEvents  = JSON.parse(data)
+
+    const orderCloseDateTimeStamp = isNaN(Date.parse(orderEvents._id.orderCloseDate)) == true ? 0 : Date.parse(orderEvents._id.orderCloseDate)
+    let orderCloseDateOverride = orderEvents._id.orderCloseDate
+    console.log("orderCloseDateOverride", orderCloseDateOverride)
+    orderEvents.events.every(item =>{
+        if(item.event.eventPromChannelStores.storeList.hasOwnProperty("orderCloseDateOverride")){
+            const currentDateString = item.event.eventPromChannelStores.storeList.orderCloseDateOverride
+            let currentDateTimeStamp = isNaN(Date.parse(currentDateString)) == true ? 0 : Date.parse(currentDateString)
+            console.log("currentDateString", currentDateString)
+            if(currentDateTimeStamp > orderCloseDateTimeStamp){
+                orderCloseDateOverride = currentDateString
+                return false
+            }
+            else return true
+        }
+    })
+
+    console.log("orderCloseDateOverride", orderCloseDateOverride)
+})
