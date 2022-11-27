ffa25a3585783a757fba916bb0ec3c978efc789c
hieu.pham <hieu.pham@empired.com>
2022-11-27 11:22:32 +0700

    sort orderevents by orderCloseDateOverride

--- wf-promo-so-validator-new/header-validatior-main.js
+++ wf-promo-so-validator-new/header-validatior-main.js
@@ -1,17 +1,20 @@
 const orderEvents = workflowContext.actions.Parse_JSON_orderEvents.outputs.body[0]
 
+//sort orderCloseDateOverride
+orderEvents.events.sort((a, b) => (Date.parse(b.event.eventPromChannelStores.storeList.orderCloseDateOverride) - Date.parse(a.event.eventPromChannelStores.storeList.orderCloseDateOverride)))
+ 
 const orderCloseDateTimeStamp = isNaN(Date.parse(orderEvents._id.orderCloseDate)) == true ? 0 : Date.parse(orderEvents._id.orderCloseDate)
 let orderCloseDateOverride = orderEvents._id.orderCloseDate
-orderEvents.events.every(item =>{
-    if(item.event.eventPromChannelStores.storeList.hasOwnProperty("orderCloseDateOverride")){
-        const currentDateString = item.event.eventPromChannelStores.storeList.orderCloseDateOverride
-        let currentDateTimeStamp = isNaN(Date.parse(currentDateString)) == true ? 0 : Date.parse(currentDateString)
-        if(currentDateTimeStamp > orderCloseDateTimeStamp){
-            orderCloseDateOverride = currentDateString
-            return false
-        }
-        else return true
+let biggestCloseDateOverride = orderEvents.events[0]
+if(biggestCloseDateOverride.event.eventPromChannelStores.storeList.hasOwnProperty("orderCloseDateOverride")){
+    const currentDateString = biggestCloseDateOverride.event.eventPromChannelStores.storeList.orderCloseDateOverride
+    let currentDateTimeStamp = isNaN(Date.parse(currentDateString)) == true ? 0 : Date.parse(currentDateString)
+    if(currentDateTimeStamp > orderCloseDateTimeStamp){
+        orderCloseDateOverride = currentDateString
     }
-})
+}
 
-return orderCloseDateOverride
+return {
+    orderCloseDateOverride,
+    orderEvents
+}
--- wf-promo-so-validator-new/header-validator.js
+++ wf-promo-so-validator-new/header-validator.js
@@ -6,22 +6,19 @@ fs.readFile("./data-test-kit/events-gus.json","utf8",(err, data) =>{
         return;
     }
     var orderEvents  = JSON.parse(data)[0]
+    //sort orderCloseDateOverride
+    orderEvents.events.sort((a, b) => (Date.parse(b.event.eventPromChannelStores.storeList.orderCloseDateOverride) - Date.parse(a.event.eventPromChannelStores.storeList.orderCloseDateOverride)))
 
     const orderCloseDateTimeStamp = isNaN(Date.parse(orderEvents._id.orderCloseDate)) == true ? 0 : Date.parse(orderEvents._id.orderCloseDate)
     let orderCloseDateOverride = orderEvents._id.orderCloseDate
-    console.log("orderCloseDateOverride", orderCloseDateOverride)
-    orderEvents.events.every(item =>{
-        if(item.event.eventPromChannelStores.storeList.hasOwnProperty("orderCloseDateOverride")){
-            const currentDateString = item.event.eventPromChannelStores.storeList.orderCloseDateOverride
-            let currentDateTimeStamp = isNaN(Date.parse(currentDateString)) == true ? 0 : Date.parse(currentDateString)
-            console.log("currentDateString", currentDateString)
-            if(currentDateTimeStamp > orderCloseDateTimeStamp){
-                orderCloseDateOverride = currentDateString
-                return false
-            }
-            else return true
+    let biggestCloseDateOverride = orderEvents.events[0]
+    if(biggestCloseDateOverride.event.eventPromChannelStores.storeList.hasOwnProperty("orderCloseDateOverride")){
+        const currentDateString = biggestCloseDateOverride.event.eventPromChannelStores.storeList.orderCloseDateOverride
+        let currentDateTimeStamp = isNaN(Date.parse(currentDateString)) == true ? 0 : Date.parse(currentDateString)
+        if(currentDateTimeStamp > orderCloseDateTimeStamp){
+            orderCloseDateOverride = currentDateString
         }
-    })
-
-    console.log("orderCloseDateOverride", orderCloseDateOverride)
+    }
 })
+
+  
