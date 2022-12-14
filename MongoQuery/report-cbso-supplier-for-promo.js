4ace8a337f5b4dbe72726eced0ebd5bc0b6ee2bf
Thuong Nguyen <m-thuongnn@tihg.onmicrosoft.com>
2022-12-14 16:52:34 +0700

    done query command for supplier-sbso-promo

--- MongoQuery/report-cbso-supplier-for-promo.js
+++ MongoQuery/report-cbso-supplier-for-promo.js
@@ -18,5 +18,53 @@ db.getCollection("staging").aggregate([
                 "$push": "$$ROOT"
             }
         }
+    },
+    {
+        "$group": {
+            "_id": {
+                "eventCode": "$_id.eventCode",
+                "primarySupplier": "$_id.primarySupplier",
+                "cbState": "$_id.cbState",
+                "storeNumber": "$_id.storeNumber"
+            },
+            "storeNumberGroups":{
+                "$addToSet": {
+                    "poNumber": "$_id.poNumber",
+                    "storeNumberGroup": "$stores"
+                }
+            }
+        }
+    },
+    {
+        "$group":{
+            "_id": {
+                "eventCode": "$_id.eventCode",
+                "primarySupplier": "$_id.primarySupplier",
+                "cbState": "$_id.cbState"
+            },
+            "data":{
+                "$addToSet": {
+                    "storeNumber": "$_id.storeNumber",
+                    "stores": "$storeNumberGroups"
+                }
+            }
+        }
+    },
+    {
+        "$project":{
+            "data.stores.storeNumberGroup._id": 0,
+            "data.stores.storeNumberGroup.orderCreated": 0,
+            "data.stores.storeNumberGroup.productSEQ": 0,
+            "data.stores.storeNumberGroup.itemCode": 0,
+            "data.stores.storeNumberGroup.orderCreated": 0,
+            "data.stores.storeNumberGroup.inserted": 0,
+            "data.stores.storeNumberGroup.processing": 0,
+            "data.stores.storeNumberGroup.storePCode": 0,
+            "data.stores.storeNumberGroup.storeBrand": 0,
+            "data.stores.storeNumberGroup.cbState": 0,
+            "data.stores.storeNumberGroup.skuCategory": 0,
+            "data.stores.storeNumberGroup.consolidationDate": 0,
+            "data.stores.storeNumberGroup.suppMinOrd": 0        
+        }
     }
 ])
