1d75cd72f9af658fdf75e9a7266f6b5d75fdcfb6
Thuong Nguyen <m-thuongnn@tihg.onmicrosoft.com>
2022-12-26 14:35:32 +0700

    done report ihg

--- MongoQuery/report-cbso-ihg-detail.js
+++ MongoQuery/report-cbso-ihg-detail.js
@@ -1,9 +1,9 @@
 db.getCollection("staging").aggregate([
     {
         "$match":{
-            "orderType": "Promo",
+            "orderType": "CNC",
             "docNo": {
-                "$in": [10000007, 10000008, 10000012]
+                "$in": [10000007, 10000008, 10000009]
             }
         }
     },
@@ -13,23 +13,60 @@ db.getCollection("staging").aggregate([
                "$sum": {
                     "$multiply": ["$orderBTax", "$qtyOrdered"]
                 }
-            },
-            "unitCost": "$orderBTax"
+            }
+        }
+    },
+    {
+        "$project": {
+            "eventCode": 1,
+            "eventDescription": "$eventDesc",
+            "documentNumber": "$docNo",
+            "supplierNumber": "$primarySupplier",
+            "supplierName": "$supName",
+            "contact": "$contactName",
+            "phone": "$contactPhone",
+            "emailAddress": "$contactEmail",
+            "emailSent": "$processedDate",
+            "stockDeliveryFrom": "$notBeforeDate",
+            "stockDeliveryTo": "$notAfterDate",
+            "item": "$itemCode",
+            "vpn": 1,
+            "apn": "$upc",
+            "description": "$skuDesc",
+            "category": "$skuCategory",
+            "store": "$storeNumber",
+            "storeName": 1,
+            "storeBrand": 1,
+            "storeRef": "$poNumber",
+            "orderCreatedDate": "$orderCreated",
+            "quantityOrdered": "$qtyOrdered",
+            "uom" : 1,
+            "unitCost": "$orderBTax",
+            "totalLine": {
+                "$trunc": ["$totalLine", 2]
+            }
         }
     },
     {
         "$group":{
             "_id": {
-                "primarySupplier": "$primarySupplier",
-                "storeNumber": "$storeNumber"
+                "supplierNumber": "$supplierNumber",
+                "store": "$store"
             },
             "stores": {
                 "$push": "$$ROOT"
             },
             "supplierStoreTotal": {
-                "$sum": {
-                    "$multiply": "$totalLine"
-                }
+                "$sum": "$totalLine"
+            }
+        }
+    },
+    {
+        "$project":{
+            "_id": 1,
+            "stores": 1,
+            "supplierStoreTotal": {
+                "$trunc": ["$supplierStoreTotal", 2]
             }
         }
     }
