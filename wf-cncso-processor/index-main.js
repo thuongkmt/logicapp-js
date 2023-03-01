face0a80ba0d5df98a3e575b69b073250b647da3
Thuong Nguyen <m-thuongnn@tihg.onmicrosoft.com>
2023-03-01 09:34:59 +0700

    update logic

--- wf-cncso-processor/index-main.js
+++ wf-cncso-processor/index-main.js
@@ -20,68 +20,73 @@ orderLines.map(orderLine =>{
     else{
         let itemLoopCount = 0
         //check itemCode
-        orderEvents.itemLists.every(item =>{
-            itemLoopCount ++
-            if(orderLine.itemCode === item.itemID){
-                orderLine.promSource = item.itemPromRegions.promSource
-                    //check at least one item.itemPromRegions.promSource equal to promSource of W
-                    switch(item.itemPromRegions.promSource){
-                        case WAREHOUSE: 
-                            warehouseOrderLines.push(orderLine)
-                            isPromotion = true
-                            break
-
-                        case CHARGEBACK:
-                            orderLine.warehouseId = item.itemPromRegions.supplierID
-                            orderLine.uom = item.itemPromRegions.salesUOM
-                            
-                            //get the promBreak#_Qty data
-                            item.itemPromPricing.every(itemPromPrice => {
-                                if(stores.promPriceLUKey === itemPromPrice.promPriceLUKey ){
-                                    //sort the promBreaks_Qty array
-                                    let promBreaks_Qty = []
-                                    for(let i=1; i<=6; i++){
-                                        promBreaks_Qty.push(itemPromPrice[`promBreak${i}_Qty`] !== undefined ? itemPromPrice[`promBreak${i}_Qty`] : 0)
-                                    }
-                                    promBreaks_Qty.sort((a,b) => {return b-a});
-                                    
-                                    //compare the orderLine.quantityOrdered vs the biggest promBreak#_Qty
-                                    if(orderLine.quantityOrdered >= promBreaks_Qty[0]){
+        if(orderEvents.length >0){
+            orderEvents.itemLists.every(item =>{
+                itemLoopCount ++
+                if(orderLine.itemCode === item.itemID){
+                    orderLine.promSource = item.itemPromRegions.promSource
+                        //check at least one item.itemPromRegions.promSource equal to promSource of W
+                        switch(item.itemPromRegions.promSource){
+                            case WAREHOUSE: 
+                                warehouseOrderLines.push(orderLine)
+                                isPromotion = true
+                                break
+    
+                            case CHARGEBACK:
+                                orderLine.warehouseId = item.itemPromRegions.supplierID
+                                orderLine.uom = item.itemPromRegions.salesUOM
+                                
+                                //get the promBreak#_Qty data
+                                item.itemPromPricing.every(itemPromPrice => {
+                                    if(stores.promPriceLUKey === itemPromPrice.promPriceLUKey ){
+                                        //sort the promBreaks_Qty array
+                                        let promBreaks_Qty = []
                                         for(let i=1; i<=6; i++){
-                                            if(itemPromPrice[`promBreak${i}_Qty`] === promBreaks_Qty[0]){
-                                                orderLine.totalLinesAmountAfterTax = (itemPromPrice[`promCost${i}_AT`] * orderLine.quantityOrdered).toFixed(2)
-                                                orderLine.costBeforeTax = itemPromPrice[`promCost${i}_BT`] 
-                                                return false;
+                                            promBreaks_Qty.push(itemPromPrice[`promBreak${i}_Qty`] !== undefined ? itemPromPrice[`promBreak${i}_Qty`] : 0)
+                                        }
+                                        promBreaks_Qty.sort((a,b) => {return b-a});
+                                        
+                                        //compare the orderLine.quantityOrdered vs the biggest promBreak#_Qty
+                                        if(orderLine.quantityOrdered >= promBreaks_Qty[0]){
+                                            for(let i=1; i<=6; i++){
+                                                if(itemPromPrice[`promBreak${i}_Qty`] === promBreaks_Qty[0]){
+                                                    orderLine.totalLinesAmountAfterTax = (itemPromPrice[`promCost${i}_AT`] * orderLine.quantityOrdered).toFixed(2)
+                                                    orderLine.costBeforeTax = itemPromPrice[`promCost${i}_BT`] 
+                                                    return false;
+                                                }
                                             }
                                         }
+    
+                                        return false
                                     }
-
-                                    return false
-                                }
-
-                                return true
-                            })
-
-                            chargeBackOrderLines.push(orderLine)
-                            break
-
-                        default: 
-                            break
-                    }
-                    if(item.itemPromRegions.promSource === WAREHOUSE){
-                        isPromotion = true
-                    }
-
-                return false
-            }
-            else{
-                if(itemLoopCount === orderEvents.itemLists.length){
-                    orderLine.promSource = WAREHOUSE
-                    warehouseOrderLines.push(orderLine)
+    
+                                    return true
+                                })
+    
+                                chargeBackOrderLines.push(orderLine)
+                                break
+    
+                            default: 
+                                break
+                        }
+                        if(item.itemPromRegions.promSource === WAREHOUSE){
+                            isPromotion = true
+                        }
+    
+                    return false
                 }
-            }
-            return true
-        })
+                else{
+                    if(itemLoopCount === orderEvents.itemLists.length){
+                        orderLine.promSource = WAREHOUSE
+                        warehouseOrderLines.push(orderLine)
+                    }
+                }
+                return true
+            })
+        }
+        else{
+            orderLine.promSource = WAREHOUSE
+        }
     }
 })
 
--- wf-cncso-processor/index.js
+++ wf-cncso-processor/index.js
@@ -38,65 +38,71 @@ fs.readFile("./data-test-kit/orderevents.json", "utf8", (error, data) =>{
                 else{
                     let itemLoopCount = 0
                     //check itemCode
-                    orderEvents.itemLists.every(item =>{
-                        itemLoopCount ++
-                        if(orderLine.itemCode === item.itemID){
-                            orderLine.promSource = item.itemPromRegions.promSource
-                                //check at least one item.itemPromRegions.promSource equal to promSource of W
-                                switch(item.itemPromRegions.promSource){
-                                    case WAREHOUSE: 
-                                        isPromotion = true
-                                        break
-
-                                    case CHARGEBACK:
-                                        orderLine.warehouseId = item.itemPromRegions.supplierID
-                                        orderLine.uom = item.itemPromRegions.salesUOM
-                                        
-                                        //get the promBreak#_Qty data
-                                        item.itemPromPricing.every(itemPromPrice => {
-                                            if(stores.promPriceLUKey === itemPromPrice.promPriceLUKey ){
-                                                //sort the promBreaks_Qty array
-                                                let promBreaks_Qty = []
-                                                for(let i=1; i<=6; i++){
-                                                    promBreaks_Qty.push(itemPromPrice[`promBreak${i}_Qty`] !== undefined ? itemPromPrice[`promBreak${i}_Qty`] : 0)
-                                                }
-                                                promBreaks_Qty.sort((a,b) => {return b-a});
-                                    
-                                                //compare the orderLine.quantityOrdered vs the biggest promBreak#_Qty
-                                                if(orderLine.quantityOrdered >= promBreaks_Qty[0]){
+                    if(orderEvents.length >0){
+                        orderEvents.itemLists.every(item =>{
+                            itemLoopCount ++
+                            if(orderLine.itemCode === item.itemID){
+                                orderLine.promSource = item.itemPromRegions.promSource
+                                    //check at least one item.itemPromRegions.promSource equal to promSource of W
+                                    switch(item.itemPromRegions.promSource){
+                                        case WAREHOUSE: 
+                                            isPromotion = true
+                                            break
+    
+                                        case CHARGEBACK:
+                                            orderLine.warehouseId = item.itemPromRegions.supplierID
+                                            orderLine.uom = item.itemPromRegions.salesUOM
+                                            
+                                            //get the promBreak#_Qty data
+                                            item.itemPromPricing.every(itemPromPrice => {
+                                                if(stores.promPriceLUKey === itemPromPrice.promPriceLUKey ){
+                                                    //sort the promBreaks_Qty array
+                                                    let promBreaks_Qty = []
                                                     for(let i=1; i<=6; i++){
-                                                        if(itemPromPrice[`promBreak${i}_Qty`] === promBreaks_Qty[0]){
-                                                            orderLine.totalLinesAmountAfterTax = (itemPromPrice[`promCost${i}_AT`] * orderLine.quantityOrdered).toFixed(2)
-                                                            orderLine.costBeforeTax = itemPromPrice[`promCost${i}_BT`] 
-                                                            return false;
+                                                        promBreaks_Qty.push(itemPromPrice[`promBreak${i}_Qty`] !== undefined ? itemPromPrice[`promBreak${i}_Qty`] : 0)
+                                                    }
+                                                    promBreaks_Qty.sort((a,b) => {return b-a});
+                                        
+                                                    //compare the orderLine.quantityOrdered vs the biggest promBreak#_Qty
+                                                    if(orderLine.quantityOrdered >= promBreaks_Qty[0]){
+                                                        for(let i=1; i<=6; i++){
+                                                            if(itemPromPrice[`promBreak${i}_Qty`] === promBreaks_Qty[0]){
+                                                                orderLine.totalLinesAmountAfterTax = (itemPromPrice[`promCost${i}_AT`] * orderLine.quantityOrdered).toFixed(2)
+                                                                orderLine.costBeforeTax = itemPromPrice[`promCost${i}_BT`] 
+                                                                return false;
+                                                            }
                                                         }
                                                     }
+    
+                                                    return false
                                                 }
-
-                                                return false
-                                            }
-
-                                            return true
-                                        })
-                                        break
-
-                                    default: 
-                                        break
-                                }
-                                if(item.itemPromRegions.promSource === WAREHOUSE){
-                                    isPromotion = true
-                                }
-
-                            return false
-                        }
-                        else{
-                            if(itemLoopCount === orderEvents.itemLists.length){
-                                orderLine.promSource = WAREHOUSE
-                                warehouseOrderLines.push(orderLine)
+    
+                                                return true
+                                            })
+                                            break
+    
+                                        default: 
+                                            break
+                                    }
+                                    if(item.itemPromRegions.promSource === WAREHOUSE){
+                                        isPromotion = true
+                                    }
+    
+                                return false
                             }
-                        }
-                        return true
-                    })
+                            else{
+                                if(itemLoopCount === orderEvents.itemLists.length){
+                                    orderLine.promSource = WAREHOUSE
+                                    warehouseOrderLines.push(orderLine)
+                                }
+                            }
+                            return true
+                        })
+                    }
+
+                    else{
+                        orderLine.promSource = WAREHOUSE
+                    }
                 }
             })
             fs.writeFile("./data-result/orderlines.json", JSON.stringify(orderLines), (error) => {
